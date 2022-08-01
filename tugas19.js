deb5c647859c273838606de9c958948a50d019bd
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 13:08:24 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+let kata = "Semangat...";
+console.log(kata.repeat(5));
+
+console.log("*".repeat(5));
+console.log("*".repeat(25));
+console.log("*".repeat(50));
+console.log("*".repeat(75));
+console.log("*".repeat(125));
+console.log("*".repeat(75));
+console.log("*".repeat(50));
+console.log("*".repeat(25));
+console.log("*".repeat(5));
+
+
+
+
